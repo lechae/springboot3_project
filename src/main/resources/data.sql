0ee8bc34bf8373204e86062e35dc61613ac56eeb
lechae <lechae55@gmail.com>
2024-03-19 18:06:39 +0900

    7.3.2 Article 뷰 컨트롤러까지 완료

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 -- 1 ~ 5장 실습 후 주석처리
-
 -- INSERT INTO member(id, name) VALUES (1, 'name 1')
 -- INSERT INTO member(id, name) VALUES (2, 'name 2')
 -- INSERT INTO member(id, name) VALUES (3, 'name 3')
+
+INSERT INTO article (title, content, created_at, update_at) VALUES ('제목 1', '내용 1', NOW(), NOW())
+INSERT INTO article (title, content, created_at, update_at) VALUES ('제목 2', '내용 2', NOW(), NOW())
+INSERT INTO article (title, content, created_at, update_at) VALUES ('제목 3', '내용 3', NOW(), NOW())
